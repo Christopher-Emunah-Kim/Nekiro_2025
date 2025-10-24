cb01a1386e2f7a5910ebb4b015a2ecc4aef605c5
Christopher-Emunah-Kim <hashin.chris@gmail.com>
2025-10-24 12:54:47 +0900

    <feat> add default components

---
+++ Source/Nekiro/Components/K_StatusComp.cpp
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "K_StatusComp.h"
+
+// Sets default values for this component's properties
+UK_StatusComp::UK_StatusComp()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UK_StatusComp::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+void UK_StatusComp::InitializeComponent ()
+{
+	Super::InitializeComponent ();
+	// ...
+}
+
+
+// Called every frame
+void UK_StatusComp::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
