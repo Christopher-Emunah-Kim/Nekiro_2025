c3293687f15638d87b952889dbba6a8785530bc2
Christopher-Emunah-Kim <hashin.chris@gmail.com>
2025-10-24 13:57:36 +0900

    <feat> load data assets to Player

--- Source/Nekiro/Character/K_Player.cpp
+++ Source/Nekiro/Character/K_Player.cpp
@@ -7,9 +7,15 @@ AK_Player::AK_Player()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	StatusComp = CreateDefaultSubobject<UK_StatusComp> ( TEXT ( "StatusComp" ) );
-	ActionComp = CreateDefaultSubobject<UK_ActionComp> ( TEXT ( "ActionComp" ) );
+	m_statusComp = CreateDefaultSubobject<UK_StatusComp> ( TEXT ( "StatusComp" ) );
+	m_actionComp = CreateDefaultSubobject<UK_ActionComp> ( TEXT ( "ActionComp" ) );
 
+	//Data Assets
+	m_damageDataAsset = TSoftObjectPtr<UK_DamageData> ( FSoftObjectPath ( TEXT ( "/Game/Blueprints/DataAssets/DA_PlayerDamage.DA_PlayerDamage'" ) ) );
+	m_comboDataAsset = TSoftObjectPtr<UK_ComboData> ( FSoftObjectPath ( TEXT ( "/Game/Blueprints/DataAssets/DA_PlayerCombo.DA_PlayerCombo'" ) ) );
+	m_defenseDataAsset = TSoftObjectPtr<UK_DefenseData> ( FSoftObjectPath ( TEXT ( "/Game/Blueprints/DataAssets/DA_PlayerDefense.DA_PlayerDefense'" ) ) );
+	m_movementDataAsset = TSoftObjectPtr<UK_MovementData> ( FSoftObjectPath ( TEXT ( "/Game/Blueprints/DataAssets/DA_PlayerMovement.DA_PlayerMovement'" ) ) );
+	m_postureDataAsset = TSoftObjectPtr<UK_PostureData> ( FSoftObjectPath ( TEXT ( "//Game/Blueprints/DataAssets/DA_PlayerPosture.DA_PlayerPosture'" ) ) );
 }
 
 // Called when the game starts or when spawned
--- Source/Nekiro/Character/K_Player.h
+++ Source/Nekiro/Character/K_Player.h
@@ -7,9 +7,9 @@
 
 #include "Nekiro/Components/K_StatusComp.h"
 #include "Nekiro/Components/K_ActionComp.h"
+#include "Nekiro/Data/K_DataAssets.h"
 
 #include "K_Player.generated.h"
-
 
 
 UCLASS()
@@ -32,18 +32,39 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	UFUNCTION ( BlueprintPure , Category = "Sekiro|Components" )
-	UK_StatusComp* GetStatusComp () const { return StatusComp; }
+	UFUNCTION ( BlueprintPure , Category = "NEKIRO|Components" )
+	UK_StatusComp* GetStatusComp () const { return m_statusComp; }
 
-	UFUNCTION ( BlueprintPure , Category = "Sekiro|Components" )
-	UK_ActionComp* GetActionComp () const { return ActionComp; }
+	UFUNCTION ( BlueprintPure , Category = "NEKIRO|Components" )
+	UK_ActionComp* GetActionComp () const { return m_actionComp; }
+
 
 
 private:
-	UPROPERTY ( VisibleAnywhere , BlueprintReadOnly , Category = "Sekiro|Components" , meta = (AllowPrivateAccess = "true") )
-	class UK_StatusComp* StatusComp;
+	//Components
 
-	UPROPERTY ( VisibleAnywhere , BlueprintReadOnly , Category = "Sekiro|Components" , meta = (AllowPrivateAccess = "true") )
-	class UK_ActionComp* ActionComp;
+
+	UPROPERTY ( VisibleAnywhere , BlueprintReadOnly , Category = "NEKIRO|Components" , meta = (AllowPrivateAccess = "true") )
+	class UK_StatusComp* m_statusComp;
+
+	UPROPERTY ( VisibleAnywhere , BlueprintReadOnly , Category = "NEKIRO|Components" , meta = (AllowPrivateAccess = "true") )
+	class UK_ActionComp* m_actionComp;
+
+
+	//Data Assets
+	UPROPERTY ( EditDefaultsOnly , BlueprintReadOnly , Category = "NEKIRO|DataAssets" , meta = (AllowPrivateAccess = "true" , ToolTip = "Player Damage Data Asset") )
+	TSoftObjectPtr<UK_DamageData> m_damageDataAsset;
+
+	UPROPERTY ( EditDefaultsOnly , BlueprintReadOnly , Category = "NEKIRO|DataAssets" , meta = (AllowPrivateAccess = "true" , ToolTip = "Player Combo Data Asset") )
+	TSoftObjectPtr<UK_ComboData> m_comboDataAsset;
+
+	UPROPERTY ( EditDefaultsOnly , BlueprintReadOnly , Category = "NEKIRO|DataAssets" , meta = (AllowPrivateAccess = "true" , ToolTip = "Player Defense Data Asset") )
+	TSoftObjectPtr<UK_DefenseData> m_defenseDataAsset;
+
+	UPROPERTY ( EditDefaultsOnly , BlueprintReadOnly , Category = "NEKIRO|DataAssets" , meta = (AllowPrivateAccess = "true" , ToolTip = "Player Movement Data Asset") )
+	TSoftObjectPtr<UK_MovementData> m_movementDataAsset;
+
+	UPROPERTY ( EditDefaultsOnly , BlueprintReadOnly , Category = "NEKIRO|DataAssets" , meta = (AllowPrivateAccess = "true" , ToolTip = "Player Posture Data Asset") )
+	TSoftObjectPtr<UK_PostureData> m_postureDataAsset;
 
 };
