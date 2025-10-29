9ece9fb0a39938a20dd729c9bd64dfafd23a0d11
Christopher-Emunah-Kim <hashin.chris@gmail.com>
2025-10-29 16:53:54 +0900

    <feat> add Boss Character, AIController, AnimInstance class

---
+++ Source/Nekiro/Character/K_BossAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "K_BossAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEKIRO_API AK_BossAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
